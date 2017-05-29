39b86aeea16a9439db57f54b98feab71a5fefa8a
Fernando Paredes <nano@fdp.io>
2017-05-29 07:46:43 -0700

    Display list of Pods

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 'use strict'
 const alfy = require('alfy')
+const R = require('ramda')
 
-alfy.output([
-  {
-    title: 'Unicorn',
-    subtitle: alfy.input
+const q = alfy.input
+
+const queryURL = (query) => `https://search.cocoapods.org/api/v1/pods.picky.hash.json?query=${query}&ids=10&offset=0&sort=quality`
+
+const getPods = R.compose(
+  R.nth(5),
+  R.head,
+  R.prop('allocations')
+)
+
+const podStanza = pod => `pod '${pod.id}'`
+
+const mapToCell = pods => pods.map(pod => ({
+  title: `${pod.id} (${pod.version})`,
+  subtitle: pod.summary,
+  arg: pod.link,
+  icon: {
+    path: ' '
+  },
+  text: {
+    copy: podStanza(pod)
   }
-])
+}))
+
+const noItemCell = q => [{
+  title: 'No Pods Found',
+  subtitle: 'Open CocoaPods Search',
+  arg: `https://cocoapods.org/?q=${q}`
+}]
+
+const isNotEmpty = R.compose(R.not, R.isEmpty)
+
+const displayCells = R.ifElse(
+  isNotEmpty,
+  mapToCell,
+  R.always(noItemCell(q))
+)
+
+alfy
+  .fetch(queryURL(q))
+  .then(getPods)
+  .then(displayCells)
+  .then(alfy.output)
+  .catch(error => {
+    alfy.output(noItemCell(q))
+  })
--- test.js
+++ test.js
@@ -1,14 +1,32 @@
 import test from 'ava'
 import alfyTest from 'alfy-test'
+import { podStanza } from './index'
 
-test(async t => {
+test('Empty Results', async t => {
   const alfy = alfyTest()
-  const result = await alfy('Rainbow')
+  const query = 'REDFASFDLAKJ;FAS'
+  const result = await alfy(query)
 
   t.deepEqual(result, [
     {
-      title: 'Unicorn',
-      subtitle: 'Rainbow'
+      title: 'No Pods Found',
+      subtitle: 'Open CocoaPods Search',
+      arg: `https://cocoapods.org/?q=${query}`
     }
   ])
+})
+
+test('Valid Results', async t => {
+  const alfy = alfyTest()
+  const query = 'Alamofire'
+  const result = await alfy(query)
+
+  const alamofire = result[0]
+
+  t.snapshot(alamofire)
+})
+
+test('Pod Stanza', () => {
+  const pod = { id: 'Alamofire' }
+  t.deepEqual(podStanza(pod), `pod '${pod.id}'`)
 })
